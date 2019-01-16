427cec66f9ed79dcfe247d0ff039a80a9959a8f4
Clay Rabenda <claytonrabenda@gmail.com>
2019-01-16 22:24:53 +0800

    point at QA.

--- utils/Context.js
+++ utils/Context.js
@@ -1,6 +1,2 @@
-// can't use 127.0.0.1 for dev if we want to test on device.
-//export let context = {host: 'http://192.168.1.5:3001'};
-//export let context = {host: 'http://202.182.117.196', port: "3001", socketPort: "3002"};
-export let context = {host: 'http://192.168.1.5', port: "3001", socketPort: "3002"};
-
-//export let context = {host: 'http://localhost:3001'};
+export let context = {host: 'http://202.182.117.196', port: "3001", socketPort: "3002"};
+//export let context = {host: 'http://192.168.1.5', port: "3001", socketPort: "3002"};
