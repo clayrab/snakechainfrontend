8952108c00e74146d3345789b592983eb5289e1b
Muhammad Ali <muhammad797@hotmail.com>
2019-06-17 15:13:56 +0500

    Gameover overlay done

--- components/GameOverOverlay.js
+++ components/GameOverOverlay.js
@@ -5,7 +5,7 @@ import {
   StyleSheet,
   Text,
   TouchableOpacity,
-  View,
+  View,ScrollView
 } from 'react-native';
 import {Font} from 'expo';
 import CONSTANTS from '../Constants.js';
@@ -15,15 +15,20 @@ import {formatToken} from '../utils/uiHelperFunctions.js';
 export default class GameOverOverlay extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      buttonDynamicStyle: {}
+    };
   }
 
   async componentDidMount() {
     await Font.loadAsync({
       'riffic-free-bold': require('../assets/fonts/RifficFree-Bold.ttf'),
     });
-    styles.buttonText = {
-      fontFamily: 'riffic-free-bold'
-    }
+    this.setState({
+      buttonDynamicStyle: {
+        fontFamily: 'riffic-free-bold',
+      }
+    });
   }
 
   render() {
@@ -31,64 +36,178 @@ export default class GameOverOverlay extends React.Component {
       return null;
     } else {
       // Level and Time
-      // <ImageBackground source={require('../assets/gameover/darkLevelBG.png')} style={styles.darkLevelBG} resizeMode="contain">
-      //   <Text style={[styles.buttonText, styles.levelText]}>Level: 13</Text>
-      //   <Text style={[styles.buttonText, styles.levelText]}>Time: 5:00</Text>
-      // </ImageBackground>
+      {/*<ImageBackground source={require('../assets/gameover/darkLevelBG.png')} style={styles.darkLevelBG} resizeMode="contain">*/}
+      {/*  <Text style={[styles.buttonText, styles.levelText]}>Level: 13</Text>*/}
+      {/*  <Text style={[styles.buttonText, styles.levelText]}>Time: 5:00</Text>*/}
+      {/*</ImageBackground>*/}
       return (
         <View style={styles.container}>
-          <ImageBackground source={require('../assets/BG.png')} style={styles.backgroundImage} resizeMode="stretch">
-            <Text style={[styles.buttonText, styles.gameOverText]}>
+          <ImageBackground source={require('../assets/gameover/gameoveroverlayback.png')} style={styles.backgroundImage} resizeMode="stretch">
+
+            <ImageBackground source={require('../assets/gameover/topgameover.png')} style={styles.topgameover}
+                             resizeMode="stretch">
+            <Text style={[styles.buttonText, styles.gameOverText,this.state.buttonDynamicStyle]}>
               GAME OVER
             </Text>
+            </ImageBackground>
             {this.props.offerContract &&
             <View style={{alignItems: "center"}}>
-              <Text style={[styles.buttonText, styles.selectionText]}>
-                You mined <Text
-                style={[styles.buttonText, styles.numberText]}>{this.props.gameOverInfo.score}</Text> gold!
-              </Text>
+
               <View style={styles.contractView}>
-                <Image source={require('../assets/gameover/yellowsnake.png')} style={styles.leftSnakeImage}
+                <Image source={require('../assets/gameover/gameoversnake.png')} style={styles.leftSnakeImage}
                        resizeMode="contain"/>
-                <ImageBackground source={require('../assets/gameover/contractSigned.png')} style={styles.contractImage}
-                                 resizeMode="contain">
-                  <Text style={[styles.buttonText, styles.contractText]}>
-                    CONTRACT
-                  </Text>
-                  <Text style={[styles.buttonText, styles.contractDetails]}>
-                    Exchange for minted snek coin now.{"\n"}{"\n"}
-                    <Text style={[styles.buttonText, styles.contractDetails, {fontSize: normalize(18),},]}>
-                      {formatToken(this.props.miningPrice,"ETH")} ETH.
+                <View >
+                  <ImageBackground source={require('../assets/gameover/collectgoldback.png')} style={styles.contractImage}
+                                   resizeMode="contain">
+                    <Text style={[styles.buttonText, styles.scoreText,this.state.buttonDynamicStyle]}>
+                      1,205
                     </Text>
-                  </Text>
-                </ImageBackground>
-                <Image source={require('../assets/gameover/greensnake.png')} style={styles.rightSnakeImage}
-                       resizeMode="contain"/>
+                  </ImageBackground>
+                  <ImageBackground source={require('../assets/gameover/scoreboard.png')} style={styles.rightSnakeImage}
+                         resizeMode="contain">
+                    <View style={styles.scoreboardView}>
+                      <View style={styles.dataView}>
+                        <Text style={[styles.buttonText, styles.scoreText1,this.state.buttonDynamicStyle]}>
+                          Time:
+                        </Text>
+                        <Text style={[styles.buttonText, styles.scoreText2,this.state.buttonDynamicStyle]}>
+                          35:00
+                        </Text>
+                      </View>
+                      <View style={styles.dataView}>
+                        <Text style={[styles.buttonText, styles.scoreText1,this.state.buttonDynamicStyle]}>
+                          Level:
+                        </Text>
+                        <Text style={[styles.buttonText, styles.scoreText2,this.state.buttonDynamicStyle]}>
+                          105
+                        </Text>
+                      </View>
+                      <View style={styles.dataView}>
+                        <View style={{flexDirection:'row'}}>
+                        <Image source={require('../assets/gameover/mashrom.png')} style={styles.mashroomImage}
+                               resizeMode="contain" />
+                        <Text style={[styles.buttonText, styles.scoreText1,this.state.buttonDynamicStyle]}>
+                          Collected:
+                        </Text>
+                        </View>
+                        <Text style={[styles.buttonText, styles.scoreText2,this.state.buttonDynamicStyle]}>
+                          92
+                        </Text>
+
+                      </View>
+                    </View>
+                  </ImageBackground>
+                </View>
+
               </View>
-              <TouchableOpacity style={[styles.touchableButton, styles.smallTouchableButton]}
-                                onPress={this.props.onDoContract}>
-                <ImageBackground source={require('../assets/gameover/yellowButton.png')}
-                                 style={styles.signConractButton} resizeMode="stretch">
-                  <Text style={[styles.buttonText, styles.signConractText]}>
-                    SIGN CONTRACT
-                  </Text>
-                </ImageBackground>
-              </TouchableOpacity>
+
+
+                  <View style={styles.scrollView}>
+                    <ImageBackground source={require('../assets/gameover/scrollback.png')}
+                                     style={styles.signConractButton} resizeMode="contain">
+                        <View style={styles.bonusView}>
+                    <ScrollView  showsVerticalScrollIndicator={false} scrollEventThrottle={2}>
+                    <View style={styles.scrollitemtop}>
+                    <ImageBackground source={require('../assets/gameover/scrollitem.png')} style={styles.scrollitem}
+                                     resizeMode="stretch">
+                      <View style={styles.scrollitemtxt}>
+                        <Text style={[styles.buttonText, styles.scoreText6,this.state.buttonDynamicStyle]}>
+                          Level 35 Bonus:
+                        </Text>
+                        <View style={styles.scrollmash}>
+                        <Text style={[styles.buttonText, styles.scoreText3,this.state.buttonDynamicStyle]}>
+                          3
+                        </Text>
+                        <Image source={require('../assets/gameover/mashrom.png')} style={styles.mashroomImage1}
+                               resizeMode="contain" />
+                        </View>
+                      </View>
+                    </ImageBackground>
+                    </View>
+                    <View style={styles.scrollitems}>
+                    <ImageBackground source={require('../assets/gameover/scrollitem.png')} style={styles.scrollitem1}
+                                     resizeMode="stretch">
+                      <View style={styles.scrollitemtxt}>
+                        <Text style={[styles.buttonText, styles.scoreText6,this.state.buttonDynamicStyle]}>
+                          Gold Mashroom:
+                        </Text>
+                        <View style={styles.scrollmash}>
+                          <Text style={[styles.buttonText, styles.scoreText4,this.state.buttonDynamicStyle]}>
+                            0.02
+                          </Text>
+                          <Image source={require('../assets/gameover/mashrom.png')} style={styles.mashroomImage1}
+                                 resizeMode="contain" />
+                        </View>
+                      </View>
+                    </ImageBackground>
+                    </View>
+                      <View style={styles.scrollitems}>
+                        <ImageBackground source={require('../assets/gameover/scrollitem.png')} style={styles.scrollitem1}
+                                         resizeMode="stretch">
+                          <View style={styles.scrollitemtxt}>
+                            <Text style={[styles.buttonText, styles.scoreText6,this.state.buttonDynamicStyle]}>
+                              Level 30 Bonus:
+                            </Text>
+                            <View style={styles.scrollmash}>
+                              <Text style={[styles.buttonText, styles.scoreText3,this.state.buttonDynamicStyle]}>
+                                3
+                              </Text>
+                              <Image source={require('../assets/gameover/mashrom.png')} style={styles.mashroomImage1}
+                                     resizeMode="contain" />
+                            </View>
+                          </View>
+
+                        </ImageBackground>
+                      </View>
+                        <View style={styles.scrollitems}>
+                            <ImageBackground source={require('../assets/gameover/scrollitem.png')} style={styles.scrollitem1}
+                                             resizeMode="stretch">
+                                <View style={styles.scrollitemtxt}>
+                                    <Text style={[styles.buttonText, styles.scoreText6,this.state.buttonDynamicStyle]}>
+                                        Level 30 Bonus:
+                                    </Text>
+                                    <View style={styles.scrollmash}>
+                                        <Text style={[styles.buttonText, styles.scoreText3,this.state.buttonDynamicStyle]}>
+                                            3
+                                        </Text>
+                                        <Image source={require('../assets/gameover/mashrom.png')} style={styles.mashroomImage1}
+                                               resizeMode="contain" />
+                                    </View>
+                                </View>
+
+                            </ImageBackground>
+                        </View>
+                    </ScrollView>
+                        </View>
+                    </ImageBackground>
+                  </View>
+
+
+
             </View>
             }
             <TouchableOpacity style={styles.touchableButton} onPress={this.props.restart}>
               <ImageBackground source={require('../assets/gameover/greenButton.png')} style={styles.largeButton}
                                resizeMode="stretch">
+                <View style={{flexDirection:'row'}}>
                 <Text style={[styles.buttonText, styles.largeButtonBText]}>
                   PLAY AGAIN
                 </Text>
+                <View style={styles.btnagain}>
+                  <Text style={[styles.buttonText, styles.scoreText5,this.state.buttonDynamicStyle]}>
+                    -3
+                  </Text>
+                  <Image source={require('../assets/gameover/mashrom.png')} style={styles.mashroomImage1}
+                         resizeMode="contain" />
+                </View>
+                </View>
               </ImageBackground>
             </TouchableOpacity>
             <TouchableOpacity style={styles.touchableButton} onPress={this.props.exit}>
               <ImageBackground source={require('../assets/gameover/darkButton.png')} style={styles.largeButton}
                                resizeMode="stretch">
                 <Text style={[styles.buttonText, styles.largeButtonText]}>
-                  GO TO MAIN MENU
+                  SELECT LEVEL
                 </Text>
               </ImageBackground>
             </TouchableOpacity>
@@ -114,8 +233,10 @@ var styles = StyleSheet.create({
     alignItems: 'center',
   },
   backgroundImage: {
-    width: screenWidth * 685 / 724,
-    height: screenHeight * 1180 / 1287,
+    // width: screenWidth * 685 / 724,
+    // height: screenHeight * 1180 / 1287,
+      width: screenWidth,
+      height: screenHeight,
     position: 'relative',
     flexDirection: 'column',
     alignItems: 'center',
@@ -142,27 +263,57 @@ var styles = StyleSheet.create({
   contractView: {
     flexDirection: 'row'
   },
-
+    scoreView:{
+    flexDirection:'column'
+    },
+  scoreboardView:{
+    flexDirection:'column',
+      marginTop:10,
+  },
+  dataView: {
+    flexDirection: 'row',
+    justifyContent:'space-around',
+    padding:5
+  },
+  leftSnakeImage: {
+    width: screenWidth * 507 / 1080,
+    height: (1050 / 943) * screenWidth * 507 / 1080,
+    marginRight: -screenWidth * 40 / 1080,
+    marginLeft: -screenWidth * 30 / 1080,
+    marginTop: (450 / 943) * screenWidth * 90 / 1080,
+    zIndex: 100,
+  },
+    mashroomImage:{
+      width:15,
+      height:15,
+      marginTop:2
+    },
+  mashroomImage1:{
+    width:16,
+    height:16,
+    marginTop:4
+  },
   contractImage: {
-    height: 508 / 362 * screenWidth * 359 / 1080,
-    width: screenWidth * 359 / 1080,
-    flexDirection: 'column',
+    height: 508 / 862 * screenWidth * 479 / 1080,
+    width: screenWidth * 549 / 1080,
+    marginLeft: -screenWidth * 80 / 1080,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  leftSnakeImage: {
-    width: screenWidth * 407 / 1080,
-    height: (950 / 943) * screenWidth * 407 / 1080,
-    marginRight: -screenWidth * 40 / 1080,
-    marginLeft: -screenWidth * 100 / 1080,
-    marginTop: (950 / 943) * screenWidth * 90 / 1080,
-    zIndex: 100,
-  },
   rightSnakeImage: {
-    width: screenWidth * 294 / 1080,
-    height: (994 / 717) * screenWidth * 294 / 1080,
-    marginLeft: -screenWidth * 40 / 1080,
-    marginTop: (950 / 943) * screenWidth * 90 / 1080,
+    width: screenWidth * 550 / 1080,
+    height: (994 / 1517) * screenWidth * 550 / 1080,
+    marginLeft: -screenWidth * 80 / 1080,
+    marginTop: -screenWidth * 40 / 1080,
+  },
+  topgameover: {
+    width: screenWidth * 850 / 1080,
+    //width: screenWidth * 0.75,
+    //height: 60,
+    height: (406 / 1625) * screenWidth * 850 / 1080,
+    justifyContent: 'center',
+    alignItems: 'center',
+      marginTop: -screenWidth * 50 / 1080,
   },
   touchableButton: {
     alignItems: 'center',
@@ -170,8 +321,8 @@ var styles = StyleSheet.create({
   },
   signConractButton: {
     marginTop: 10,
-    width: screenWidth * 411 / 1080,
-    height: (294 / 1000) * screenWidth * 411 / 1080,
+    width: screenWidth * 1080 / 1080,
+    height: (694 / 1000) * screenWidth * 1080 / 1080,
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -187,6 +338,40 @@ var styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+    scrollitem:{
+      width: screenWidth * 780 / 1080,
+      height: (350 / 1625) * screenWidth * 780 / 1080,
+      marginRight:40,
+    },
+  scrollitem1:{
+    width: screenWidth * 780 / 1080,
+    height: (350 / 1625) * screenWidth * 780 / 1080,
+    marginRight:40,
+  },
+    scrollView:{
+      flexDirection:'column',
+    },
+    bonusView:{
+        height:205,
+        paddingTop:20,
+        marginTop:10,
+        width:280
+    },
+    scrollitemtop:{
+       marginBottom:10,
+      // marginVertical: 45,
+      //   marginLeft:15
+    },
+    scrollitems:{
+      marginBottom:10,
+        // marginLeft:15
+    },
+    scrollitemtxt:{
+      flexDirection:'row',
+      justifyContent:'space-between',
+      paddingTop: 10,
+      marginRight:5
+    } ,
   buttonText: {
     fontWeight: 'bold'
   },
@@ -200,7 +385,41 @@ var styles = StyleSheet.create({
   },
   gameOverText: {
     color: "#fab523",
-    fontSize: normalize(29),
+    fontSize: normalize(32),
+    marginBottom:10
+  },
+  scoreText:{
+    color: "#fab523",
+    fontSize: normalize(30),
+    marginBottom:10,
+    marginRight:60
+  },
+    scoreText1:{
+      color: "#fab523",
+      fontSize: normalize(15),
+    },
+    scoreText2:{
+      color: "white",
+      fontSize: normalize(15),
+    },
+  scoreText3:{
+    color: "#fab523",
+    fontSize: normalize(18),
+  },
+    scoreText6:{
+        color: "#fab523",
+        fontSize: normalize(18),
+        marginLeft:10
+    },
+  scoreText4:{
+    color: "#fab523",
+    fontSize: normalize(18),
+    marginLeft:20
+  },
+  scoreText5:{
+    color: "black",
+    fontSize: normalize(18),
+    marginLeft:20
   },
   levelText: {
     color: "#fab523",
@@ -230,5 +449,13 @@ var styles = StyleSheet.create({
   },
   smallTouchableButton: {
     marginBottom: 50
-  }
+  },
+    btnagain:{
+        flexDirection:'row',
+        marginLeft:35
+    },
+    scrollmash:{
+        flexDirection:'row',
+        marginRight:7
+    }
 });
